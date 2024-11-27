e04a48c59df37b96fe9d0ffcf6b5eb8c5d9c478b
Pontax02 <ppontanilla07@gmail.com>
2024-11-27 10:49:49 +0100

    feat(Pagina reflex): :art: Añadir encabezado a la página

--- who__is_who/who__is_who.py
+++ who__is_who/who__is_who.py
@@ -2,5 +2,14 @@ import reflex as rx
 
 from rxconfig import config
 
+
+def index():
+    return rx.heading("Bienvenido al Who is Who",font_size="10px" , as_="h1")
+
+
+
+
 app = rx.App()
 app.add_page(index)
+
+
