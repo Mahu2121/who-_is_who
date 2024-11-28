e66e792d26a3ef116fd3955ef8e58ba1eefe98ea
Hugo Gonzalez Besada <hugogbmahu@gmail.com>
2024-11-28 13:32:44 +0100

    feat(añadiendo un chat en la historia de usuario2): :memo:

--- who__is_who/who__is_who.py
+++ who__is_who/who__is_who.py
@@ -1,13 +1,20 @@
 import reflex as rx
 
+from who__is_who import style
+
 from rxconfig import config
 
+def chat() -> rx.Component:
+    return rx.vstack(
+        rx.input(placeholder="Haz una pregunta sobre tu personaje en mente", style=style.chat_style),
+        margin_top="850px"
+    )
 
 def index():
-    return rx.heading("Bienvenido al Who is Who",font_size="10px" , as_="h1")
-
-
-
+    return rx.container(
+    rx.heading("Bienvenido al Who is Who",font_size="10px" , as_="h1"),
+    chat()
+    )
 
 app = rx.App()
 app.add_page(index)
