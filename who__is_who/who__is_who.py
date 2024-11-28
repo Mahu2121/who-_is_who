64339287d148baaa2b02c99b259c364bf7cf6b81
Pontax02 <ppontanilla07@gmail.com>
2024-11-28 13:30:39 +0100

    feat(tablero): :art: crear tablero provisional en el que poner las imagenes

--- who__is_who/who__is_who.py
+++ who__is_who/who__is_who.py
@@ -4,9 +4,20 @@ from rxconfig import config
 
 
 def index():
-    return rx.heading("Bienvenido al Who is Who",font_size="10px" , as_="h1")
-
-
+    return rx.box(rx.heading("Bienvenido al Who is Who",font_size="10px" , as_="h1"),
+            rx.grid(
+                rx.foreach(
+                rx.Var.range(24),lambda i: rx.card(height="28vh", width="140px" ),
+                    
+                    ),
+                    columns="8",    
+                    spacing="3",
+                    width="65%",
+                    margin_left="230px",
+                    
+                    
+            )
+    )
 
 
 app = rx.App()
