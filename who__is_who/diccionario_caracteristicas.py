5b98b5749e4d332aabc66002ccf56bf75421e9de
Pontax02 <ppontanilla07@gmail.com>
2024-11-29 18:27:07 +0100

    refactor(diccionario): :art: refactorizar la funcion diccionario

--- who__is_who/diccionario_caracteristicas.py
+++ who__is_who/diccionario_caracteristicas.py
@@ -1,13 +1,12 @@
 import pytest
-                        #genero, color pelo ,pelo largo/pelo corto, gafas, barba,bigote, sombrero        
+                        #genero, color pelo ,pelo largo/pelo corto, gafas, barba,bigote, sombrero
 def diccionario():
-    
-    diccionario={"Susan":["mujer","blanco", "largo"], "Claire":["mujer","pelirroja","corto","gafas","sombrero",], "David":["hombre","rubio","corto","barba"], "Anne":["mujer","castaño","corto","pendientes"],
-                "Robert":["hombre","castaño","corto"], "Anita":["mujer","rubia","largo"], "Joe":["hombre","rubio","corto","gafas"], "George":["hombre","blanco","corto","sombrero"], 
+
+    return {"Susan":["mujer","blanco", "largo"], "Claire":["mujer","pelirroja","corto","gafas","sombrero",], "David":["hombre","rubio","corto","barba"], "Anne":["mujer","castaño","corto","pendientes"],
+                "Robert":["hombre","castaño","corto"], "Anita":["mujer","rubia","largo"], "Joe":["hombre","rubio","corto","gafas"], "George":["hombre","blanco","corto","sombrero"],
                 "Bill": ["hombre","pelirrojo","semicalvo","perilla"], "Alfred":["hombre","pelirrojo","largo","bigote"], "Max":["hombre","castaño","corto","bigote"],"Tom":["hombre","castaño","semicalvo","gafas"],
                 "Alex": ["hombre","castaño","corto","bigote"], "Sam": ["hombre","blanco","semicalvo","gafas"], "Richard": ["hombre","castaño","semicalvo","barba"], "Paul":["hombre","blanco","corto","gafas"] ,
                 "Maria":["mujer","castaño","largo","boina","pendientes"], "Frans":["hombre","pelirrojo","corto"], "Herman": ["hombre", "pelirrojo", "semicalvo"], "Bernard":["hombre","castaño","corto","gorro"],
                 "Philip":["hombre","castaño","corto","barba"],"Eric":["hombre","rubio","corto","gorra"], "Charles":["hombre","rubio","corto","bigote"], "Peter":["hombre","blanco","corto"]}
-    
 
 
