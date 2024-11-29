7e64aee9282ff6a8cf6872d49646e0b0460d2344
Pontax02 <ppontanilla07@gmail.com>
2024-11-29 18:32:40 +0100

    feat: :white_check_mark: comprobar random
    comprobar que la funcion randomizer funciona(posteriormente se realizaran casos test)

--- who__is_who/test_randomizer.py
+++ who__is_who/test_randomizer.py
@@ -1,12 +1,19 @@
-import pytest
-from who__is_who.randomizer import diccionario
+import random
+import reflex as rx
+from randomizer import randomizer
+
+
+personas={"Susan":0,"Claire":0,"David":0,"Anne":0,"Robert":0,"Anita":0,"Joe":0,"George":0,"Bill":0,"Alfred":0,"Max":0,"Tom":0,"Alex":0,"Sam":0,"Richard":0,"Paul":0,"Maria":0,"Frans":0,"Herman":0,"Bernard":0,"Phillip":0,"Eric":0,"Charles":0,"Peter":0,}
+
 def hacer_random():
-    personas={"Susan":0,"Claire":0,"David":0,"Anne":0,"Robert":0,"Anita":0,"Joe":0,"George":0,"Bill":0,"Alfred":0,"Max":0,"Tom":0,"Alex":0,"Sam":0,"Richard":0,"Paul":0,"Maria":0,"Frans":0,"Herman":0,"Bernard":0,"Phillip":0,"Eric":0,"Charles":0,"Peter":0,}
-    contador=0
-    while i < 30:
-
-        resultado=random.choice(list(personas.keys()))
+    
+        return random.choice(list(personas.keys()))
+        
+        
+for x in range(100):
+        
+        resultado = hacer_random()
         personas[resultado] +=1
-        contador+=1
-    return print(resultado)
+        print(resultado)
 
+print(personas)
