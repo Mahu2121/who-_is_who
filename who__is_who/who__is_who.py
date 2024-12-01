41b974901738c55f5df7d416a744615c4c13da20
Hugo González Besada <hugogbmahu@gmail.com>
2024-12-01 23:27:52 +0100

    feat(creando una clase self): Con esto se puede almacenar los valores del chat que pueden actualizarse al clicar el boton

--- who__is_who/who__is_who.py
+++ who__is_who/who__is_who.py
@@ -4,18 +4,36 @@ from who__is_who import style
 
 from rxconfig import config
 
+
+class ChatState(rx.State):
+    text: str = ""
+
+    @rx.event
+    def update_text(self, new_text:str):
+        self.text = new_text
+
+
 def chat() -> rx.Component:
     return rx.container(
-    rx.hstack(
-        rx.input(placeholder = "Haz una pregunta sobre tu personaje en mente", style = style.chat_style),
-        rx.button("Enviar", style = style.button_style),
-    ),margin_top = "90%",
+        rx.hstack(
+
+            rx.input(default_value=ChatState.text, 
+                placeholder = "Haz una pregunta sobre tu personaje en mente",   
+                on_change=ChatState.update_text,
+                style = style.chat_style,
+                ),
+
+            rx.button("Enviar", 
+                on_click=lambda: ChatState.update_text(ChatState.text),
+                style = style.button_style,
+                ),
+        ),margin_top = "80%",
     )
 
 def index():
     return rx.container(
     rx.heading("Bienvenido al Who is Who",font_size = "10px" , as_ = "h1"),
-    chat()
+    chat(),
     )
 
 app = rx.App()
