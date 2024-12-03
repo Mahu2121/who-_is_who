aa498c5afc0b46aef8dff35f362c0b2310abe5cd
Hugo González Besada <hugogbmahu@gmail.com>
2024-12-04 00:10:20 +0100

    feat:

--- who__is_who/reconocer_caracteristicas.py
+++ who__is_who/reconocer_caracteristicas.py
@@ -2,3 +2,14 @@ from who__is_who import ChatState
 
 from diccionario_caracteristicas import diccionario
 
+
+
+def reconocer_caracteristicas():
+
+    imput_chat = ChatState.get_form_data()
+    for caracteristica in diccionario:
+
+        try caracteristica.index(caracteristica):
+
+
+        
