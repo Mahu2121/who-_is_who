640799a6492bec06ade81dbbc0e162c98f2bc667
Hugo González Besada <hugogbmahu@gmail.com>
2024-12-03 23:32:05 +0100

    feat(creando un classmethod para obtener form_data):

--- who__is_who/who__is_who.py
+++ who__is_who/who__is_who.py
@@ -12,6 +12,10 @@ class ChatState(rx.State):
     def update_text(self, form_data: dict):
         print(form_data)
         self.form_data = form_data
+    
+    @classmethod
+    def get_form_data(cls):
+        return cls.form_data
 
 
 
