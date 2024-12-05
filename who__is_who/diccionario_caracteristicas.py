99f583decb59ab27c07ae3770b457ad6ed22c501
Hugo Gonzalez Besada <hugogbmahu@gmail.com>
2024-12-05 10:17:25 +0100

    refactor:

--- who__is_who/diccionario_caracteristicas.py
+++ who__is_who/diccionario_caracteristicas.py
@@ -2,11 +2,27 @@
                         #genero, color pelo ,pelo largo/pelo corto, gafas, barba,bigote, sombrero
 def diccionario():
 
-    return {"Susan":["mujer","blanco", "largo"], "Claire":["mujer","pelirroja","corto","gafas","sombrero",], "David":["hombre","rubio","corto","barba"], "Anne":["mujer","castaño","corto","pendientes"],
-                "Robert":["hombre","castaño","corto"], "Anita":["mujer","rubia","largo"], "Joe":["hombre","rubio","corto","gafas"], "George":["hombre","blanco","corto","sombrero"],
-                "Bill": ["hombre","pelirrojo","semicalvo","perilla"], "Alfred":["hombre","pelirrojo","largo","bigote"], "Max":["hombre","castaño","corto","bigote"],"Tom":["hombre","castaño","semicalvo","gafas"],
-                "Alex": ["hombre","castaño","corto","bigote"], "Sam": ["hombre","blanco","semicalvo","gafas"], "Richard": ["hombre","castaño","semicalvo","barba"], "Paul":["hombre","blanco","corto","gafas"] ,
-                "Maria":["mujer","castaño","largo","boina","pendientes"], "Frans":["hombre","pelirrojo","corto"], "Herman": ["hombre", "pelirrojo", "semicalvo"], "Bernard":["hombre","castaño","corto","gorro"],
-                "Philip":["hombre","castaño","corto","barba"],"Eric":["hombre","rubio","corto","gorra"], "Charles":["hombre","rubio","corto","bigote"], "Peter":["hombre","blanco","corto"]}
-
-
+    return {"Susan":["mujer","blanco", "largo"], 
+            "Claire":["mujer","pelirroja","corto","gafas","sombrero",], 
+            "David":["hombre","rubio","corto","barba"], 
+            "Anne":["mujer","castaño","corto","pendientes"],
+            "Robert":["hombre","castaño","corto"], 
+            "Anita":["mujer","rubia","largo"], 
+            "Joe":["hombre","rubio","corto","gafas"], 
+            "George":["hombre","blanco","corto","sombrero"],
+            "Bill": ["hombre","pelirrojo","semicalvo","perilla"], 
+            "Alfred":["hombre","pelirrojo","largo","bigote"], 
+            "Max":["hombre","castaño","corto","bigote"],
+            "Tom":["hombre","castaño","semicalvo","gafas"],
+            "Alex": ["hombre","castaño","corto","bigote"], 
+            "Sam": ["hombre","blanco","semicalvo","gafas"], 
+            "Richard": ["hombre","castaño","semicalvo","barba"], 
+            "Paul":["hombre","blanco","corto","gafas"] ,
+            "Maria":["mujer","castaño","largo","boina","pendientes"], 
+            "Frans":["hombre","pelirrojo","corto"], 
+            "Herman": ["hombre", "pelirrojo", "semicalvo"], 
+            "Bernard":["hombre","castaño","corto","gorro"],
+            "Philip":["hombre","castaño","corto","barba"],
+            "Eric":["hombre","rubio","corto","gorra"], 
+            "Charles":["hombre","rubio","corto","bigote"], 
+            "Peter":["hombre","blanco","corto"]}
