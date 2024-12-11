3ec1e640faab24df2bea8216bad1d5724211daea
Hugo Gonzalez Besada <hugogbmahu@gmail.com>
2024-12-11 08:56:47 +0100

    Revert "refactor(eliminando el diccionario sobrante y rehaciendo las rutas que lo referenciaban):"
    This reverts commit 80c709daa201e30744a8986909103d8413bbff92.

--- who__is_who/estado.py
+++ who__is_who/estado.py
@@ -1,6 +1,6 @@
 import reflex as rx
 
-from who__is_who.diccionario_caracteristicas2 import diccionario
+from who__is_who.diccionario_caracteristicas import diccionario
 import random
 from who__is_who.randomizer import randomizer
 
@@ -13,7 +13,7 @@ class state(rx.State):
     name_diccionarioo= diccionario()
     form_data: dict = {}
     datos: dict = diccionario()
-
+    personas_eliminadas:list = []
     personaje: str = ""
     final: str = ""
     adivinaste: str = ""
@@ -42,8 +42,8 @@ class state(rx.State):
         self.depurar_personajes(form_data.get("question"))
 
     @rx.var
-    def keys(self):       #*lista de keys del diccionario*
-        return list(self.datos.keys())   #conversión de las claves de un diccionario a una lista
+    def keys(self) -> list:
+        return list(self.datos.keys())
 
     
     @rx.event
@@ -55,21 +55,28 @@ class state(rx.State):
     def depurar_personajes(self,caracteristica: str):
 
         diccionario_nuevo = {}
-        caracteristicas_personaje = self.datos.get(self.final)     #dentro del diccionario coje el personaje seleccionado por el random
+        caracteristicas_personaje = self.datos.get(self.final)
 
         print(caracteristicas_personaje)
-        if caracteristica not in caracteristicas_personaje:         #se asegura que la caractristica(imput) está dentro de del los valores del personaje seleccionado por el random
+        if caracteristica not in caracteristicas_personaje:
 
-            print("El personaje no tiene esta caracteristica")      #si no está -> printea eso
+            self.personas_eliminadas = []
+            for personas in self.datos:
+                if caracteristica in self.datos[personas]:
+                    self.personas_eliminadas.append(personas)
 
+            for personas in self.personas_eliminadas:
+                del self.datos[personas]
+
+                
             return
 
-        for personaje, caracteristicas in self.datos.items():       #bucle for por cada "key de personaje (nombre de personaje) y sus valores" en los items(key:value) del diccionario
+        for personaje, caracteristicas in self.datos.items():
 
-            if  caracteristica in caracteristicas:                  #si la caracteristica(imput) está en los valores del personaje, añade al diccionario el personaje con sus valores
+            if  caracteristica in caracteristicas:
                 diccionario_nuevo.update({personaje:caracteristicas})
 
-        self.datos = diccionario_nuevo                              #iguala el diccionario inicializado por la clase de estado al diccionario nuevo para solo almacenar los que cumplen el if (los que tienen su caracteristica dentro de los valores del personaje seleccionado por random)
+        self.datos = diccionario_nuevo
 
     
-        print(self.datos)                                       
+        print(self.datos)
--- who__is_who/randomizer.py
+++ who__is_who/randomizer.py
@@ -1,9 +1,9 @@
 import reflex as rx
 import pytest
 import random
-from diccionario_caracteristicas2 import diccionario
+from .diccionario_caracteristicas import diccionario
 
-def randomizer():
+def randomizer(diccionario):
     info_perso=diccionario()
     return random.choice(list(info_perso.keys()))
 
