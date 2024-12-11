24139d1ceaeae9f7db64766f8fbc81c263b35f78
Hugo González Besada <hugogbmahu@gmail.com>
2024-12-11 19:29:10 +0100

    test(creando el caso test para la funcion depurar_personajes): :stethoscope:

--- who__is_who/test_depurar_personajes.py
+++ who__is_who/test_depurar_personajes.py
@@ -1,23 +1,44 @@
 from diccionario_caracteristicas2 import diccionario
 
-def depurar_personajes(self) :
+def depurar_personajes(caracteristica : str):
 
+        
         datos = diccionario()
-        caracteristica_buscada = self.form_data.get("question")
 
-        encontrados = []
-        no_encontrados = []
+        diccionario_nuevo = {}
+
+        caracteristicas_personaje = datos.get("David")
+
+
+        print("caracteristicas personaje final:", caracteristicas_personaje)
+
+        if caracteristica not in caracteristicas_personaje:
+
+            personas_eliminadas = []
+            for personas in datos:
+                if caracteristica in datos[personas]:
+                    personas_eliminadas.append(personas)
+
+            for personas in personas_eliminadas:
+                del datos[personas]
+
+
 
         for personaje, caracteristicas in datos.items():
 
-            if caracteristica_buscada in caracteristicas:
-                encontrados.append(personaje)
-    
-            else:
-                no_encontrados.append(personaje)
+            if  caracteristica in caracteristicas:
+                diccionario_nuevo.update({personaje:caracteristicas})
 
-        print(encontrados)
-        print(no_encontrados)
+        datos = diccionario_nuevo
+        return datos 
+        
 
-        return encontrados, no_encontrados
-    
+if __name__ == "__main__":
+    resultado = depurar_personajes("rubio")
+    print("Test segun el imput'rubio':", resultado)
+
+    resultado2 = depurar_personajes("castaño")
+    print("Test segun el imput 'castaño':", resultado2)
+
+    resultado3 = depurar_personajes("barba")
+    print("Test segun el imput 'barba':", resultado3)
